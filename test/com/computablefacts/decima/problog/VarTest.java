bcd2c7e9585c4e5a90843422b52321da2089e873
Cyrille Savelief <csavelief@mncc.fr>
2020-04-13 08:49:24 +0200

    Split class TermTest into ConstTest and VarTest.

--- test/com/computablefacts/decima/problog/VarTest.java
+++ test/com/computablefacts/decima/problog/VarTest.java
@@ -3,31 +3,7 @@ package com.computablefacts.decima.problog;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class TermTest {
-
-  @Test
-  public void testConstEquals() {
-
-    Const a = new Const("a");
-    Const b = new Const(1);
-
-    Assert.assertEquals(a, a);
-    Assert.assertEquals(b, b);
-
-    Assert.assertNotEquals(a, b);
-  }
-
-  @Test
-  public void testConstHashcode() {
-
-    Const a = new Const("a");
-    Const b = new Const(1);
-
-    Assert.assertEquals(a.hashCode(), a.hashCode());
-    Assert.assertEquals(b.hashCode(), b.hashCode());
-
-    Assert.assertNotEquals(a.hashCode(), b.hashCode());
-  }
+public class VarTest {
 
   @Test
   public void testVarEquals() {
