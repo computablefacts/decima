bb199c0186aa2631d55bf10f79982a2a0c3253fa
Cyrille Savelief <csavelief@mncc.fr>
2020-04-14 08:10:05 +0200

    Backport tests on graphs. (2/2)

---
+++ test/com/computablefacts/decima/problog/graphs/Graph4Test.java
@@ -0,0 +1,72 @@
+package com.computablefacts.decima.problog.graphs;
+
+import static com.computablefacts.decima.problog.TestUtils.isValid;
+import static com.computablefacts.decima.problog.TestUtils.kb;
+import static com.computablefacts.decima.problog.TestUtils.parseClause;
+
+import java.math.BigDecimal;
+import java.util.Set;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.computablefacts.decima.problog.Clause;
+import com.computablefacts.decima.problog.Const;
+import com.computablefacts.decima.problog.Estimator;
+import com.computablefacts.decima.problog.InMemoryKnowledgeBase;
+import com.computablefacts.decima.problog.Literal;
+import com.computablefacts.decima.problog.Solver;
+import com.google.common.collect.Lists;
+
+/**
+ * Extracted from Theofrastos Mantadelis and Gerda Janssens (2010). "Nesting Probabilistic
+ * Inference"
+ */
+public class Graph4Test {
+
+  @Test
+  public void testGraph() {
+
+    // Create kb
+    InMemoryKnowledgeBase kb = kb();
+
+    // Init kb with facts
+    kb.azzert(parseClause("0.4::edge(a, b)."));
+    kb.azzert(parseClause("0.55::edge(a, c)."));
+    kb.azzert(parseClause("0.8::edge(b, e)."));
+    kb.azzert(parseClause("0.2::edge(b, d)."));
+    kb.azzert(parseClause("0.4::edge(c, d)."));
+    kb.azzert(parseClause("0.3::edge(e, f)."));
+    kb.azzert(parseClause("0.5::edge(d, f)."));
+    kb.azzert(parseClause("0.6::edge(d, g)."));
+    kb.azzert(parseClause("0.7::edge(f, h)."));
+    kb.azzert(parseClause("0.7::edge(g, h)."));
+
+    // Init kb with rules
+    kb.azzert(parseClause("path(X, Y) :- edge(X, Y)."));
+    kb.azzert(parseClause("path(X, Y) :- path(X, Z), edge(Z, Y)."));
+
+    // Query kb
+    // path(b, f)?
+    Solver solver = new Solver(kb);
+    Literal query = new Literal("path", new Const("b"), new Const("f"));
+    Set<Clause> proofs = solver.proofs(query);
+
+    // Verify answers
+    // path(b, f) :- 0.2::edge(b, d), 0.5::edge(d, f).
+    // path(b, f) :- 0.8::edge(b, e), 0.3::edge(e, f).
+    Assert.assertEquals(2, proofs.size());
+
+    Assert.assertTrue(
+        isValid(proofs, "path(b, f)", Lists.newArrayList("0.2::edge(b, d)", "0.5::edge(d, f)")));
+    Assert.assertTrue(
+        isValid(proofs, "path(b, f)", Lists.newArrayList("0.8::edge(b, e)", "0.3::edge(e, f)")));
+
+    // Verify BDD answer
+    // 0.316::path(b, f).
+    Estimator estimator = new Estimator(proofs);
+    BigDecimal probability = estimator.probability(query, 3);
+
+    Assert.assertTrue(BigDecimal.valueOf(0.316).compareTo(probability) == 0);
+  }
+}
---
+++ test/com/computablefacts/decima/problog/graphs/Graph5Test.java
@@ -0,0 +1,72 @@
+package com.computablefacts.decima.problog.graphs;
+
+import static com.computablefacts.decima.problog.TestUtils.isValid;
+import static com.computablefacts.decima.problog.TestUtils.kb;
+import static com.computablefacts.decima.problog.TestUtils.parseClause;
+
+import java.math.BigDecimal;
+import java.util.Set;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.computablefacts.decima.problog.Clause;
+import com.computablefacts.decima.problog.Const;
+import com.computablefacts.decima.problog.Estimator;
+import com.computablefacts.decima.problog.InMemoryKnowledgeBase;
+import com.computablefacts.decima.problog.Literal;
+import com.computablefacts.decima.problog.Solver;
+import com.google.common.collect.Lists;
+
+/**
+ * Extracted from Theofrastos Mantadelis and Gerda Janssens (2010). "Nesting Probabilistic
+ * Inference"
+ */
+public class Graph5Test {
+
+  @Test
+  public void testGraph() {
+
+    // Create kb
+    InMemoryKnowledgeBase kb = kb();
+
+    // Init kb with facts
+    kb.azzert(parseClause("0.6::edge(1, 2)."));
+    kb.azzert(parseClause("0.1::edge(1, 3)."));
+    kb.azzert(parseClause("0.4::edge(2, 5)."));
+    kb.azzert(parseClause("0.3::edge(2, 6)."));
+    kb.azzert(parseClause("0.3::edge(3, 4)."));
+    kb.azzert(parseClause("0.8::edge(4, 5)."));
+    kb.azzert(parseClause("0.2::edge(5, 6)."));
+
+    // Init kb with rules
+    kb.azzert(parseClause("path(X, Y) :- edge(X, Y)."));
+    kb.azzert(parseClause("path(X, Y) :- path(X, Z), edge(Z, Y)."));
+
+    // Query kb
+    // path(1, 6)?
+    Solver solver = new Solver(kb);
+    Literal query = new Literal("path", new Const("1"), new Const("6"));
+    Set<Clause> proofs = solver.proofs(query);
+
+    // Verify answers
+    // path(1, 6) :- 0.6::edge(1, 2), 0.3::edge(2, 6).
+    // path(1, 6) :- 0.6::edge(1, 2), 0.4::edge(2, 5), 0.2::edge(5, 6).
+    // path(1, 6) :- 0.1::edge(1, 3), 0.3::edge(3, 4), 0.8::edge(4, 5), 0.2::edge(5, 6).
+    Assert.assertEquals(3, proofs.size());
+
+    Assert.assertTrue(
+        isValid(proofs, "path(1, 6)", Lists.newArrayList("0.6::edge(1, 2)", "0.3::edge(2, 6)")));
+    Assert.assertTrue(isValid(proofs, "path(1, 6)",
+        Lists.newArrayList("0.6::edge(1, 2)", "0.4::edge(2, 5)", "0.2::edge(5, 6)")));
+    Assert.assertTrue(isValid(proofs, "path(1, 6)", Lists.newArrayList("0.1::edge(1, 3)",
+        "0.3::edge(3, 4)", "0.8::edge(4, 5)", "0.2::edge(5, 6)")));
+
+    // Verify BDD answer
+    // 0.2167296::path(1, 6).
+    Estimator estimator = new Estimator(proofs);
+    BigDecimal probability = estimator.probability(query, 7);
+
+    Assert.assertTrue(BigDecimal.valueOf(0.2167296).compareTo(probability) == 0);
+  }
+}
