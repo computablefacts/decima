dbafe8b94c2ae752f44e7e1da13ced5ff807c064
Cyrille Savelief <csavelief@mncc.fr>
2020-10-05 19:25:51 +0200

    Better tests for equals() and hashcode() for classes AbstractTerm, Var and Const.

--- test/com/computablefacts/decima/problog/ConstTest.java
+++ test/com/computablefacts/decima/problog/ConstTest.java
@@ -1,31 +1,13 @@
 package com.computablefacts.decima.problog;
 
-import org.junit.Assert;
 import org.junit.Test;
+
+import nl.jqno.equalsverifier.EqualsVerifier;
 
 public class ConstTest {
 
   @Test
-  public void testConstEquals() {
-
-    Const a = new Const("a");
-    Const b = new Const(1);
-
-    Assert.assertEquals(a, a);
-    Assert.assertEquals(b, b);
-
-    Assert.assertNotEquals(a, b);
-  }
-
-  @Test
-  public void testConstHashcode() {
-
-    Const a = new Const("a");
-    Const b = new Const(1);
-
-    Assert.assertEquals(a.hashCode(), a.hashCode());
-    Assert.assertEquals(b.hashCode(), b.hashCode());
-
-    Assert.assertNotEquals(a.hashCode(), b.hashCode());
+  public void testHashcodeAndEquals() {
+    EqualsVerifier.forClass(Const.class).withIgnoredFields("value_").verify();
   }
 }
--- test/com/computablefacts/decima/problog/VarTest.java
+++ test/com/computablefacts/decima/problog/VarTest.java
@@ -3,30 +3,14 @@ package com.computablefacts.decima.problog;
 import org.junit.Assert;
 import org.junit.Test;
 
+import nl.jqno.equalsverifier.EqualsVerifier;
+
 public class VarTest {
 
   @Test
-  public void testVarEquals() {
-
-    Var a = new Var();
-    Var b = new Var();
-
-    Assert.assertEquals(a, a);
-    Assert.assertEquals(b, b);
-
-    Assert.assertNotEquals(a, b);
-  }
-
-  @Test
-  public void testVarHashcode() {
-
-    Var a = new Var();
-    Var b = new Var();
-
-    Assert.assertEquals(a.hashCode(), a.hashCode());
-    Assert.assertEquals(b.hashCode(), b.hashCode());
-
-    Assert.assertNotEquals(a.hashCode(), b.hashCode());
+  public void testHashcodeAndEquals() {
+    EqualsVerifier.forClass(Var.class).withNonnullFields("id_").withIgnoredFields("isWildcard_")
+        .verify();
   }
 
   @Test
