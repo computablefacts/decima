004491ffdbf39e74345c11e26db0d3beb1dae2d2
Cyrille Savelief <csavelief@mncc.fr>
2021-05-06 08:33:44 +0200

    Cleanup

--- src/com/computablefacts/decima/problog/Literal.java
+++ src/com/computablefacts/decima/problog/Literal.java
@@ -147,7 +147,7 @@ final public class Literal {
         builder.append(term.toString());
       } else {
         builder.append('"');
-        builder.append(Function.encode(term.toString()).replaceAll("\r\n|\n", "\\\\n"));
+        builder.append(Function.encode(term.toString()));
         builder.append('"');
       }
     }
